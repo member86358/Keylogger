1f9a5e0de5408b1b3ae8a1f92d7e5e34cac70e9a
Ragnarsson <David.Ragnarsson@afconsult.com>
2020-10-11 21:26:14 +0200

    Upprensning, förtydligande i logfilen. Logging.info adderad istället för ett print statement

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import pynput
 from pynput.keyboard import Key, Listener, GlobalHotKeys
 from pynput import keyboard
-from pynput import mouse
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 count = 0
+if __name__ == "__main__":
+    def main():
+        def write_file(count):
+            with open("log.txt", "r+") as f:
+                f.write("Amount of times <ctrl>+l pressed: "+str(count))
+                f.close()
 
+        def on_activate():
+            logging.info("Ctrl + l pressed")
+            global count
+            count += 1
+            write_file(count)
 
-def main():
-    def write_file(count):
-        with open("log.txt", "r+") as f:
-            f.write(str(count))
-            f.close()
+        hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+l'),on_activate)
 
-    def on_activate():
-        print('Global hotkey activated!')
-        global count
-        count += 1
-        write_file(count)
+        def for_canonical(f):
+            return lambda k: f(l.canonical(k))       
 
-    hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+l'),on_activate)
-
-    def for_canonical(f):
-        return lambda k: f(l.canonical(k))       
-
-    with keyboard.Listener(on_press=for_canonical(hotkey.press),on_release=for_canonical(hotkey.release)) as l:
-        l.join()
-main()
+        with keyboard.Listener(on_press=for_canonical(hotkey.press),on_release=for_canonical(hotkey.release)) as l:
+            l.join()
+    main()
