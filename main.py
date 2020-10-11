21a1fc9fa79bd9d7bd550924cb6f329ed112b17d
Ragnarsson <David.Ragnarsson@afconsult.com>
2020-10-11 21:13:38 +0200

    skriv till log.txt antal ctrl + l

--- main.py
+++ main.py
@@ -7,36 +7,23 @@ logging.basicConfig(level=logging.DEBUG)
 count = 0
 
 
-def write_file(count):
-    with open("log.txt", "r+") as f:
-        f.write(str(count))
-        f.close()
+def main():
+    def write_file(count):
+        with open("log.txt", "r+") as f:
+            f.write(str(count))
+            f.close()
 
-def on_press(key):
-    global count
-    if (key != Key.esc):
-        if (str(key) == '\x0c'):
-            print("{0} pressed".format(key))
-            logging.debug('on_press')
-            count += 1
-            write_file(count)
+    def on_activate():
+        print('Global hotkey activated!')
+        global count
+        count += 1
+        write_file(count)
 
-def on_release(key):
-    if (key == Key.esc):
-        logging.debug('Esc')
-        return False
+    hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+l'),on_activate)
 
+    def for_canonical(f):
+        return lambda k: f(l.canonical(k))       
 
-def on_activate():
-    print('Global hotkey activated!')
-
-def for_canonical(f):
-    return lambda k: f(l.canonical(k))       
-
-hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+l'),on_activate)
-#Keyboard listener
-#with Listener(on_press=on_press, on_release=on_release) as listener:
-#    listener.join()
-
-with keyboard.Listener(on_press=for_canonical(hotkey.press),on_release=for_canonical(hotkey.release)) as l:
-    l.join()
+    with keyboard.Listener(on_press=for_canonical(hotkey.press),on_release=for_canonical(hotkey.release)) as l:
+        l.join()
+main()
